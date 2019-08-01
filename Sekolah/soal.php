4b736024860897eb89ce1dc03c1077bf77ba442e
calungankuy <hilmi_fadhilah123@yahoo.com>
2019-08-01 17:04:19 +0700

    main

---
+++ Sekolah/soal.php
@@ -0,0 +1,8 @@
+<?php
+
+$jambu =  15000*6*5;
+
+
+ echo "Hasil penjualan seluruh jambu dan dus ersebut adalah :  <br>";
+
+ ?>
---
+++ Sekolah/soal2.php
@@ -0,0 +1,6 @@
+<?php
+$tama = 1;
+$toni = 0.5;
+echo "umur toni+umur tini = $toni<br>";
+echo "$toni";
+?>
